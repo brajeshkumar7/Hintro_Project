beb43066e553ad73369fdb67431c58afcbc0d13e
Brajesh Kumar <kumarbrajesh7927@gmail.com>
2026-02-16 01:28:32 +0530

    dev: Final polish and extra features

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store/authStore.js";
+import { getSocket } from "../socket.js";
 
 export default function ProtectedRoute({ children }) {
   const token = useAuthStore((s) => s.token);
   const location = useLocation();
+
+  useEffect(() => {
+    if (token) getSocket(token);
+  }, [token]);
 
   if (!token) {
     return <Navigate to="/login" state={{ from: location }} replace />;
